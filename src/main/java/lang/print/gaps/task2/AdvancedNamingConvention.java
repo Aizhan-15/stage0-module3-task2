5bb7d20a55d42f447999df320a96d6de4e05d495
Aizhan-15 <aizhanurman@gmail.com>
2022-09-07 02:02:05 +0600

    Completed all the tasks

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,8 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    public static void main(String[] args) {
+        int adultAge = 18;
+        System.out.println(adultAge);
+    }
+    }
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,28 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        int variable1 = 2000;
         int value = 1000;
-        int variable = 4000;
+        int variable2 = 4000;
         int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int variables1 = 5000;
+        int variables2 = 7000;
+        int value3 = 90000;
+        int value4 = 100000;
+        int integer = 1000000;
+        int var = 1234567;
+        int integer2 = 1234567;
+        System.out.println(variable);
+        System.out.println(variable1);
+        System.out.println(value);
+        System.out.println(variable2);
+        System.out.println(value2);
+        System.out.println(variables1);
+        System.out.println(variables2);
+        System.out.println(value3);
+        System.out.println(value4);
+        System.out.println(integer);
+        System.out.println(var);
+        System.out.println(integer2);
     }
 }
