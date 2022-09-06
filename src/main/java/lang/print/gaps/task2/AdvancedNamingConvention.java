5eea78cd94218455da0bfe648e24376d3f044244
Aizhan-15 <aizhanurman@gmail.com>
2022-09-07 02:05:40 +0600

    Completed all the tasks

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -3,6 +3,8 @@ package lang.print.gaps.task2;
 public class AdvancedNamingConvention {
     public static void main(String[] args) {
         int adultAge = 18;
+        int age = 28;
+        System.out.println(age);
         System.out.println(adultAge);
     }
     }
